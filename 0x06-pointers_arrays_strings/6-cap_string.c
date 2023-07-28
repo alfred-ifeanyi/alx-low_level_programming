e3fb0a502d8c45d7c02de490c3b5ef7679b40df4
alfred-ifeanyi <alfredifeanyi20@gmail.com>
2023-07-28 04:37:34 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i == 0
+		if (i == 0 ||
 				str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 				str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
 				str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
