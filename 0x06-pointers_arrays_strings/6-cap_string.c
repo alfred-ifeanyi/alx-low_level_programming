eeaee53aa70bb08b0bcd0f5ce5d513f13e489bce
alfred-ifeanyi <alfredifeanyi20@gmail.com>
2023-07-28 04:13:16 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: Pointer to the input string
+ *
+ * Return: Pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == '' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+				str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+				str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+				str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+				str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] += 32;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
